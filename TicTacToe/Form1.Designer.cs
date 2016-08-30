b3d3f698965b22cbe8ee94102b4d54c7662ccd22
Darius Musiolik <MrFlyingToasterman@yahoo.de>
2016-08-30 18:58:55 +0200

    Added first build

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(82, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(64, 64);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(152, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(64, 64);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 82);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(64, 64);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "-";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(82, 82);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(64, 64);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "-";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(152, 82);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(64, 64);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "-";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 152);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(64, 64);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "-";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(82, 152);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(64, 64);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "-";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(152, 152);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(64, 64);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "-";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 219);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Player:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Location = new System.Drawing.Point(69, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "PlayerID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(170, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Restart";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 246);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TicTacToe - GUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        //Startplayer Variable, accessable from every other needed method
+        public int startplayer;
+        //Player
+        public int player;
+        //Global dataArray|Position y  x
+        String[,] data = new String[3, 3] {
+            {"", "", ""} ,  
+            {"", "", ""} , 
+            {"", "", ""} 
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Start message
+            MessageBox.Show("Choose your Player Number\nThe Computer choose a random beginner");
+
+            //Choose a random Player
+            Random rdom = new Random();
+            startplayer = rdom.Next(2)+1;
+
+            //Set who's playing now
+            player = startplayer;
+
+            //Set label2 to random choosen player
+            this.label2.Text = "" + startplayer;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Check if button was clicked before
+            if (checkarray(0, 0) == 0)
+            {
+                return;
+            }
+
+            //Sets the X or O
+            button1.Text = checkplayer();
+
+            //Transmite position data and save to global dataArray
+            add2array(0, 0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Check if button was clicked before
+            if (checkarray(0, 1) == 0)
+            {
+                return;
+            }
+
+            //Sets the X or O
+            button2.Text = checkplayer();
+
+            //Transmite position data and save to global dataArray
+            add2array(0, 1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Check if button was clicked before
+            if (checkarray(0, 2) == 0)
+            {
+                return;
+            }
+
+            //Sets the X or O
+            button3.Text = checkplayer();
+
+            //Transmite position data and save to global dataArray
+            add2array(0, 2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Check if button was clicked before
+            if (checkarray(1, 0) == 0)
+            {
+                return;
+            }
+
+            //Sets the X or O
+            button4.Text = checkplayer();
+
+            //Transmite position data and save to global dataArray
+            add2array(1, 0);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //Check if button was clicked before
+            if (checkarray(1, 1) == 0)
+            {
+                return;
+            }
+
+            //Sets the X or O
+            button5.Text = checkplayer();
+
+            //Transmite position data and save to global dataArray
+            add2array(1, 1);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //Check if button was clicked before
+            if (checkarray(1, 2) == 0)
+            {
+                return;
+            }
+
+            //Sets the X or O
+            button6.Text = checkplayer();
+
+            //Transmite position data and save to global dataArray
+            add2array(1, 2);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //Check if button was clicked before
+            if (checkarray(2, 0) == 0)
+            {
+                return;
+            }
+
+            //Sets the X or O
+            button7.Text = checkplayer();
+
+            //Transmite position data and save to global dataArray
+            add2array(2, 0);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //Check if button was clicked before
+            if (checkarray(2, 1) == 0)
+            {
+                return;
+            }
+
+            //Sets the X or O
+            button8.Text = checkplayer();
+
+            //Transmite position data and save to global dataArray
+            add2array(2, 1);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //Check if button was clicked before
+            if (checkarray(2,2) == 0)
+            {
+                return;
+            }
+
+            //Sets the X or O
+            button9.Text = checkplayer();
+
+            //Transmite position data and save to global dataArray
+            add2array(2, 2);
+        }
+
+        public int checkarray(int y, int x)
+        {
+            //Check if button was allready clicked
+            if (data[y, x] != "")
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+
+        public String checkplayer()
+        {
+            //This method checks the playerstate and returns an X or O
+            if (player == 1)
+            {
+                return "X";
+            }
+            else
+            {
+                return "O";
+            }
+        }
+
+        public void add2array(int y, int x)
+        {
+            //save the input to the global dataArray
+            data[y,x] = "" + player;
+
+            //test arrayvalue
+            //MessageBox.Show(data[y,x]);
+
+            //Check if someone has allredy won
+            checkwin();
+        }
+
+        public void checkwin()
+        {
+            //Check the dataArray
+            if (data[0, 0] == "1" && data[0, 1] == "1" && data[0, 2] == "1" || data[0, 0] == "2" && data[0, 1] == "2" && data[0, 2] == "2")
+            {
+                MessageBox.Show("Player " + player + " won!");
+                return;
+            }
+            if (data[1, 0] == "1" && data[1, 1] == "1" && data[1, 2] == "1" || data[1, 0] == "2" && data[1, 1] == "2" && data[1, 2] == "2")
+            {
+                MessageBox.Show("Player " + player + " won!");
+                return;
+            }
+            if (data[0, 2] == "1" && data[1, 1] == "1" && data[2, 0] == "1" || data[0, 2] == "2" && data[1, 1] == "2" && data[2, 0] == "2")
+            {
+                MessageBox.Show("Player " + player + " won!");
+                return;
+            }
+            if (data[2, 0] == "1" && data[2, 1] == "1" && data[2, 2] == "1" || data[2, 0] == "2" && data[2, 1] == "2" && data[2, 2] == "2")
+            {
+                MessageBox.Show("Player " + player + " won!");
+                return;
+            }
+            if (data[0, 0] == "1" && data[1, 1] == "1" && data[2, 2] == "1" || data[0, 0] == "2" && data[1, 1] == "2" && data[2, 2] == "2")
+            {
+                MessageBox.Show("Player " + player + " won!");
+                return;
+            }
+            if (data[0, 0] == "1" && data[1, 0] == "1" && data[2, 0] == "1" || data[0, 0] == "2" && data[1, 0] == "2" && data[2, 0] == "2")
+            {
+                MessageBox.Show("Player " + player + " won!");
+                return;
+            }
+            if (data[0, 1] == "1" && data[1, 1] == "1" && data[2, 1] == "1" || data[0, 1] == "2" && data[1, 1] == "2" && data[2, 1] == "2")
+            {
+                MessageBox.Show("Player " + player + " won!");
+                return;
+            }
+            if (data[0, 2] == "1" && data[1, 2] == "1" && data[2, 2] == "1" || data[0, 2] == "2" && data[1, 2] == "2" && data[2, 2] == "2")
+            {
+                MessageBox.Show("Player " + player + " won!");
+                return;
+            }
+
+            //switchplayer
+            switchplayer();
+        }
+
+        public void switchplayer()
+        {
+            //switch player
+            if (player == 1)
+            {
+                player = 2;
+            }
+            else
+            {
+                player = 1;
+            }
+
+            //Set Label
+            label2.Text = "" + player;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            //Restart
+            int i2 = 0;
+            do
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    data[i2, i] = "";
+                }
+                i2++;
+            } while (i2 < 3);
+
+            //Clean btns
+            button1.Text = "-";
+            button2.Text = "-";
+            button3.Text = "-";
+            button4.Text = "-";
+            button5.Text = "-";
+            button6.Text = "-";
+            button7.Text = "-";
+            button8.Text = "-";
+            button9.Text = "-";
+        }
+
+    }
+}
+
+/* By Darius Musiolik
+ * https://www.github.com/MrFlyingToasterman
+ * MrFlyingToasterman@yahoo.de
+ * 
+ *  In case you fork me just remove my credit.
+ * 
+*/
